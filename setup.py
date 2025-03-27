13dc8cb38380511a0d2c940c2cd75cfa0ca59949
Kanon14 <geochua144@gmail.com>
2025-03-27 23:12:42 +0800

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "mediChat",
+    version = "0.0.0",
+    author = "Kanon Chua",
+    author_email = "geochua144@gmail.com",
+    packages = find_packages(),
+    install_requires = []
+)
